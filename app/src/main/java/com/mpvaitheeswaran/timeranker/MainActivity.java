d59cdd6ffeba82b439292fe7a4206373bf14dd3e
mpvaitheeswaran <mpvaitheeswaran>
2020-10-08 17:28:36 +0530

    Initial commit

---
+++ app/src/main/java/com/mpvaitheeswaran/timeranker/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mpvaitheeswaran.timeranker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.mpvaitheeswaran.timeranker.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding=DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+    }
+}
